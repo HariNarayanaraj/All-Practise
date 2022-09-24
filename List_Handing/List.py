245dd9c5e6cbd7c7c05baf25c98f4a47d2b9916a
Hari Raj <HariNarayanaraj@users.github.com>
2022-09-24 15:02:53 +0530

    List Handling update

--- List_Handing/List.py
+++ List_Handing/List.py
@@ -1,3 +1,24 @@
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# create a list
+list1 = list((1, 2, 3, 4, 5))
+print("Original list ", list1)
 
-print("Original list ", list)
+
+class list:
+
+    def __int__(self):
+        pass
+
+    def reverse(self):
+        originalList = list1
+        list1.reverse()
+        print("Reverse for list ", list1)
+        list1.reverse()
+
+    def copy(self):
+        list2 = list1.copy()
+        print("Copy of list1 in list2 ", list2)
+
+
+obj = list()
+obj.reverse()
+obj.copy()
